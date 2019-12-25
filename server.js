12176c8d77d3f1b12eec752509035877e1e05d7e
venkat-krishnakumar <46819930+venkat-krishnakumar@users.noreply.github.com>
2019-12-25 23:38:30 +0530

    New modyfy
    Clien and build flip

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require(“path”);
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'build/client')))
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
